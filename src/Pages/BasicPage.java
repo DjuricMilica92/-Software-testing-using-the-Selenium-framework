91838bc312779b925ebb7f15df7c42831683e8f9
Milica Djuric <milicadjuricrs@gmail.com>
2022-03-06 16:46:37 +0100

    Basic Page

--- src/Pages/BasicPage.java
+++ src/Pages/BasicPage.java
@@ -1,5 +1,31 @@
 package Pages;
 
-public class BasicPage {
+import java.util.List;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public abstract class BasicPage {
+
+	protected WebDriver driver;
+	protected JavascriptExecutor js;
+	protected WebDriverWait wait;
+
+	public BasicPage(WebDriver driver, WebDriverWait wait, JavascriptExecutor js) {
+		this.driver = driver;
+		this.wait = wait;
+		this.js = (JavascriptExecutor) driver;
+	}
+
+	public WebElement getLocation() {
+		return null;
+	}
+
+	public List<WebElement> getMenuBar() {
+		return null;
+	}
+
 
 }
