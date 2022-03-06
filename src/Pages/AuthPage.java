6a5a1c4aa7137715ba5c16ecfc33b811d001d623
Milica Djuric <milicadjuricrs@gmail.com>
2022-03-06 16:47:14 +0100

    Auth page

--- src/Pages/AuthPage.java
+++ src/Pages/AuthPage.java
@@ -1,5 +1,47 @@
 package Pages;
 
-public class AuthPage {
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class AuthPage extends BasicPage {
+
+	public AuthPage(WebDriver driver, WebDriverWait wait, JavascriptExecutor js) {
+		super(driver, wait, js);
+	}
+
+	public WebElement getAuthButton() {
+		return driver.findElement(By.xpath("//*[contains(@class, 'after-arrow user-trigger-js user-trigger-active')]"));
+	}
+
+	public WebElement getMyAccountButton() {
+		return driver.findElement(By.xpath("//*[contains(@class, 'my-account-dropdown')]//li[1]/a"));
+	}
+
+	public WebElement getLogOutButton() {
+		return driver.findElement(By.xpath("//*[contains(@class, 'my-account-dropdown')]//li[2]/a"));
+	}
+
+	public void logOut() {
+		js.executeScript("arguments[0].click();", getLogOutButton());
+	}
+
+	@Override
+	public WebElement getLocation() {
+		return null;
+	}
+
+	@Override
+	public List<WebElement> getMenuBar() {
+		return null;
+	}
 
 }
+
