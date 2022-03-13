b120f113c8ae987bdef2d862e2a0ac4331d08444
Milica Djuric <milicadjuricrs@gmail.com>
2022-03-13 13:12:16 +0100

    Notification System Page

--- src/Pages/NotificationSistemPage.java
+++ src/Pages/NotificationSistemPage.java
@@ -1,5 +1,34 @@
 package Pages;
 
-public class NotificationSistemPage {
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+public class NotificationSistemPage extends BasicPage {
+	public NotificationSistemPage(WebDriver driver, WebDriverWait wait, JavascriptExecutor js) {
+		super(driver, wait, js);
+	}
+
+	public WebElement getLoginButton() {
+		return driver.findElement(By.xpath("//*[contains(@class, 'alert--success')]"));
+	}
+
+	public String getNotificationMessage() {
+		return driver.findElement(By.xpath("//*[contains(@class, 'alert--success')]")).getText();
+	}
+
+	public String getNotificationErrorMessage() {
+		return driver.findElement(By.xpath("//*[contains(@class, 'alert--danger')]")).getText();
+	}
+
+	public void waitForMessageToDisappear() throws InterruptedException {
+		wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+		wait.until(ExpectedConditions.attributeToBe(By.xpath(" //*[contains(@class, 'system_message')]"), "style",
+				"display: none;"));
+	}
 }
